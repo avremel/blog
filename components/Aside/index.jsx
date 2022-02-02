aa39aed535d210a54c2f84d1ca08e507e88570cb
Avraham Kamenetzky <avraham@ajmadison.com>
2022-02-02 12:09:35 -0500

    save

--- components/Aside/index.jsx
+++ components/Aside/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Aside = ({header, text}) => {
   return (
-    <aside>
+    <aside className='DocsMarkdown--aside' role='note' data-type='note'>
       {header && <div class="DocsMarkdown--aside-header">{header}</div>}
       <p>{text}</p>
     </aside>
