addf4353a94f96a88a9b326610a521b12e9e0464
Avraham Kamenetzky <avraham@ajmadison.com>
2022-02-02 14:05:52 -0500

    save

--- components/CodeEmbed/index.jsx
+++ components/CodeEmbed/index.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import cn from 'classnames'
 import css from './index.module.css'
 
-const CodeEmbed = ({path, linkText}) => {
-  if (linkText) {
-    return (
-      <a href={path} target="_blank" className='DocsMarkdown--link'>
-        <span className='DocsMarkdown--link-content'>{linkText}</span>
-      </a>
-    )
-  }
+const CodeEmbed = ({path}) => {
   return (
-    <div className={cn(css.wrapper, css['full-width'])}>
-      <iframe src={path} width="100%" height="500px"></iframe>
+    <div className={css.wrapper}>
+      <p>Take the code for a spin 👇</p>
+      <div className={cn(css['full-width'])}>
+        <iframe src={path} width="100%" height="600px"></iframe>
+      </div>
     </div>
   )
 }
