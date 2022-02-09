306ed314706ffa8eab7a4a45fb8751b01fe65090
Avraham Kamenetzky <avraham@ajmadison.com>
2022-02-09 12:34:17 -0500

    save

--- components/BlogMarkdown/index.jsx
+++ components/BlogMarkdown/index.jsx
@@ -1,6 +1,7 @@
 import { MDXProvider } from '@mdx-js/react'
 import cn from 'classnames'
 import { BlogLayout } from '..'
+import slugify from '../../utils/slugify'
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 hljs.registerLanguage('javascript', javascript);
@@ -28,10 +29,23 @@ const CodeBlock = ({children}) => {
   )
 }
 
+const H2 = ({children}) => {
+  const id = `${slugify(children)}`
+  return (
+    <h2 id={id}>
+      <span className='DocsMarkdown--header-anchor-positioner'>
+        <a className='DocsMarkdown--header-anchor Link Link-without-underline' href={`#${id}`} aria-hidden="true"></a>
+      </span>
+      <span>{children}</span>
+    </h2>
+  )
+}
+
 const components = {
   a: Link,
   inlineCode: InlineCode,
   code: CodeBlock,
+  h2: H2,
 }
 
 export default function Post({meta, children}) {
