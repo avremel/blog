4b2f5b8cec05bfbc953efa4fbb07fb7b5a47cc0d
Avraham Kamenetzky <avraham@ajmadison.com>
2022-02-09 12:21:59 -0500

    save

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ import {Layout} from '../components'
 const Home = () => {
   return (
     <Layout title='Avi Kaminetzky - Blog'>
-      <a href="/posts/react-flavored-js">React Flavored Javascript</a>
+      <ul>
+        <li><a href="/posts/code-challenges">100 Code Challenges</a></li>
+        <li><a href="/posts/react-flavored-js">React Flavored Javascript</a></li>
+      </ul>
+
     </Layout>
   )
 }
