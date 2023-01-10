45fdd967e6712d34f51b1182378601e69a0a5840
Avraham Kamenetzky <avraham@ajmadison.com>
2023-01-10 12:18:23 -0500

    save

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import css from './index.module.css'
 
 const Layout = ({title, children}) => {
+  const { pathname } = useRouter()
+
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
       <div className={css.page}>
+        {pathname !== '/' && (
+          <nav>
+            <a href="/">Home</a>
+          </nav>
+        )}
+
         <main className='body'>
           <div className='content' page-type='document'>
             {children}
