5da4c4a9519e24a576b5f8ff27a136c92b68b409
Avraham Kamenetzky <avraham@ajmadison.com>
2023-03-09 15:39:51 -0500

    save

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <Layout title='Avi Kaminetzky - Blog'>
       <ul>
+        <li><a href="/posts/algolia-sanity">Algolia 🤝 Sanity</a></li>
         <li><a href="/posts/endeca-to-algolia">Faceted search: From Endeca to Algolia</a></li>
         <li><a href="/posts/react-flavored-js">React Flavored Javascript</a></li>
       </ul>
