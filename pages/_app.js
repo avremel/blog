5f039967d5b4b3adfd5f9922380833ead2f66d0d
Avraham Kamenetzky <avraham@ajmadison.com>
2023-03-09 15:48:55 -0500

    save

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import 'highlight.js/styles/github.css'
 import '../styles/index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} className='DocsPage' />
+  return (
+    <>
+      <Component {...pageProps} className='DocsPage' />
+
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
