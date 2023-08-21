fd8f0d30b02c7d3c43399ca106a79d67262fc034
Avraham Kamenetzky <avraham@ajmadison.com>
2023-08-21 11:54:49 -0400

    save

--- components/BlogLayout/index.jsx
+++ components/BlogLayout/index.jsx
@@ -19,11 +19,19 @@ const BlogLayout = ({ meta, children }) => {
 
       <article
         className="
-        prose prose-code:before:hidden prose-code:after:hidden prose-code:bg-slate-100 prose-pre:bg-slate-100
-        prose-quoteless prose-blockquote:bg-slate-100 prose-blockquote:not-italic prose-blockquote:font-normal prose-blockquote:leading-normal
-        prose-a:underline prose-a:underline-offset-4 prose-a:decoration-slate-400
-      "
+          prose prose-code:before:hidden prose-code:after:hidden prose-code:bg-slate-100 prose-pre:bg-slate-100
+          prose-quoteless prose-blockquote:bg-slate-100 prose-blockquote:not-italic prose-blockquote:font-normal prose-blockquote:leading-normal
+          prose-a:underline prose-a:underline-offset-4 prose-a:decoration-slate-400
+          prose-headings:font-semibold
+          prose-h1:mb-2
+        "
       >
+        <h1>{meta.title}</h1>
+
+        <div className="bg-slate-100 text-slate-600 p-1 rounded h-fit w-fit px-3 text-center text-xs flex-shrink-0 basis-24">
+          {meta.date}
+        </div>
+
         {children}
       </article>
     </div>
